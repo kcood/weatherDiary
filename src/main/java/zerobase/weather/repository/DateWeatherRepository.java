f92e38003cb06af48d81cb220b743a530e3d08bd
DYS <kcood92@gmail.com>
2022-08-05 11:28:14 +0900

    Spring Scheduler
    -Saving Data into MysqlDB Automatically
    -Refactor createDiary

---
+++ src/main/java/zerobase/weather/repository/DateWeatherRepository.java
@@ -0,0 +1,16 @@
+package zerobase.weather.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import zerobase.weather.domain.DateWeather;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findAllByDate(LocalDate localDate);
+
+}
